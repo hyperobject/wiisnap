d21e5d5478a98f44790e7ae298f75ea2e5d1ba2e
Technoboy10 <technoboy10@fake.email>
2013-03-12 14:55:52 -0600

    Finished adding all functions for WiiSnap. v1.0 complete.

--- WiiSnap.py
+++ WiiSnap.py
@@ -18,9 +18,10 @@ class CORSHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 			wm.rumble = 1
 		elif setr == 'off':
 			wm.rumble = 0
-	"""elif 'getled' in path:
+	elif 'getled' in path:
+		led = wm.state['led']
 		f = open(ospath + '/return', 'w+')
-		f.write(str(wm.state['led']))
+		f.write(str(led))
 		f.close()
 		f = open(ospath + '/return', 'rb')
 		ctype = self.guess_type(ospath + '/return')
@@ -30,7 +31,173 @@ class CORSHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	        self.send_header("Content-Length", str(fs[6]))
 	        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
 	        self.send_header("Access-Control-Allow-Origin", "*")
-	        self.end_headers()"""
+	        self.end_headers()
+		return f
+	elif 'wmx' in path:
+		x = wm.state['acc'][0]
+		f = open(ospath + '/return', 'w+')
+		f.write(str(x))
+		f.close()
+		f = open(ospath + '/return', 'rb')
+		ctype = self.guess_type(ospath + '/return')
+		self.send_response(200)
+	        self.send_header("Content-type", ctype)
+	        fs = os.fstat(f.fileno())
+	        self.send_header("Content-Length", str(fs[6]))
+	        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+	        self.send_header("Access-Control-Allow-Origin", "*")
+	        self.end_headers()
+		return f
+	elif 'wmy' in path:
+		y = wm.state['acc'][1]
+		f = open(ospath + '/return', 'w+')
+		f.write(str(y))
+		f.close()
+		f = open(ospath + '/return', 'rb')
+		ctype = self.guess_type(ospath + '/return')
+		self.send_response(200)
+	        self.send_header("Content-type", ctype)
+	        fs = os.fstat(f.fileno())
+	        self.send_header("Content-Length", str(fs[6]))
+	        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+	        self.send_header("Access-Control-Allow-Origin", "*")
+	        self.end_headers()
+		return f
+	elif 'wmz' in path:
+		z = wm.state['acc'][2]
+		f = open(ospath + '/return', 'w+')
+		f.write(str(z))
+		f.close()
+		f = open(ospath + '/return', 'rb')
+		ctype = self.guess_type(ospath + '/return')
+		self.send_response(200)
+	        self.send_header("Content-type", ctype)
+	        fs = os.fstat(f.fileno())
+	        self.send_header("Content-Length", str(fs[6]))
+	        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+	        self.send_header("Access-Control-Allow-Origin", "*")
+	        self.end_headers()
+		return f
+	elif 'battery' in path:
+		battery = wm.state['battery']
+		f = open(ospath + '/return', 'w+')
+		f.write(str(battery))
+		f.close()
+		f = open(ospath + '/return', 'rb')
+		ctype = self.guess_type(ospath + '/return')
+		self.send_response(200)
+	        self.send_header("Content-type", ctype)
+	        fs = os.fstat(f.fileno())
+	        self.send_header("Content-Length", str(fs[6]))
+	        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+	        self.send_header("Access-Control-Allow-Origin", "*")
+	        self.end_headers()
+		return f
+	elif 'buttons' in path:
+		buttons = wm.state['buttons']
+		f = open(ospath + '/return', 'w+')
+		f.write(str(buttons))
+		f.close()
+		f = open(ospath + '/return', 'rb')
+		ctype = self.guess_type(ospath + '/return')
+		self.send_response(200)
+	        self.send_header("Content-type", ctype)
+	        fs = os.fstat(f.fileno())
+	        self.send_header("Content-Length", str(fs[6]))
+	        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+	        self.send_header("Access-Control-Allow-Origin", "*")
+	        self.end_headers()
+		return f
+	elif 'nunchukbtn' in path:
+		buttons = wm.state['nunchuk']['buttons']
+		f = open(ospath + '/return', 'w+')
+		f.write(str(buttons))
+		f.close()
+		f = open(ospath + '/return', 'rb')
+		ctype = self.guess_type(ospath + '/return')
+		self.send_response(200)
+	        self.send_header("Content-type", ctype)
+	        fs = os.fstat(f.fileno())
+	        self.send_header("Content-Length", str(fs[6]))
+	        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+	        self.send_header("Access-Control-Allow-Origin", "*")
+	        self.end_headers()
+		return f
+	elif 'nunchukx' in path:
+		x = wm.state['nunchuk']['acc'][0]
+		f = open(ospath + '/return', 'w+')
+		f.write(str(x))
+		f.close()
+		f = open(ospath + '/return', 'rb')
+		ctype = self.guess_type(ospath + '/return')
+		self.send_response(200)
+	        self.send_header("Content-type", ctype)
+	        fs = os.fstat(f.fileno())
+	        self.send_header("Content-Length", str(fs[6]))
+	        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+	        self.send_header("Access-Control-Allow-Origin", "*")
+	        self.end_headers()
+		return f
+	elif 'nunchuky' in path:
+		y = wm.state['nunchuk']['acc'][1]
+		f = open(ospath + '/return', 'w+')
+		f.write(str(y))
+		f.close()
+		f = open(ospath + '/return', 'rb')
+		ctype = self.guess_type(ospath + '/return')
+		self.send_response(200)
+	        self.send_header("Content-type", ctype)
+	        fs = os.fstat(f.fileno())
+	        self.send_header("Content-Length", str(fs[6]))
+	        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+	        self.send_header("Access-Control-Allow-Origin", "*")
+	        self.end_headers()
+		return f
+	elif 'nunchukz' in path:
+		z = wm.state['nunchuk']['acc'][2]
+		f = open(ospath + '/return', 'w+')
+		f.write(str(z))
+		f.close()
+		f = open(ospath + '/return', 'rb')
+		ctype = self.guess_type(ospath + '/return')
+		self.send_response(200)
+	        self.send_header("Content-type", ctype)
+	        fs = os.fstat(f.fileno())
+	        self.send_header("Content-Length", str(fs[6]))
+	        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+	        self.send_header("Access-Control-Allow-Origin", "*")
+	        self.end_headers()
+		return f
+	elif 'nunchukstickx' in path:
+		x = wm.state['nunchuk']['stick'][0]
+		f= open(ospath + '/return', 'w+')
+		f.write(str(x))
+		f.close()
+		f = open(ospath + '/return', 'rb')
+		ctype = self.guess_type(ospath + '/return')
+		self.send_response(200)
+	        self.send_header("Content-type", ctype)
+	        fs = os.fstat(f.fileno())
+	        self.send_header("Content-Length", str(fs[6]))
+	        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+	        self.send_header("Access-Control-Allow-Origin", "*")
+	        self.end_headers()
+		return f
+	elif 'nunchuksticky' in path:
+		y = wm.state['nunchuk']['stick'][1]
+		f= open(ospath + '/return', 'w+')
+		f.write(str(y))
+		f.close()
+		f = open(ospath + '/return', 'rb')
+		ctype = self.guess_type(ospath + '/return')
+		self.send_response(200)
+	        self.send_header("Content-type", ctype)
+	        fs = os.fstat(f.fileno())
+	        self.send_header("Content-Length", str(fs[6]))
+	        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+	        self.send_header("Access-Control-Allow-Origin", "*")
+	        self.end_headers()
+		return f
 if __name__ == "__main__":
     print "WiiSnap by Technoboy10"
     import re
@@ -40,10 +207,7 @@ if __name__ == "__main__":
     print "Please connect a Wiimote by pressing 1 and 2 on your Wiimote."
     #try:
     wm = cwiid.Wiimote()
-    wm.rpt_mode = cwiid.RPT_BTN
-    #except:
-        #print "Wiimote not found. Please press 1 and 2 on your Wiimote and try again."
-        #quit()
+    wm.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC | cwiid.RPT_MOTIONPLUS | cwiid.RPT_NUNCHUK
     PORT = 1280 #Major device code for a Wiimote
 
     Handler = CORSHTTPRequestHandler
